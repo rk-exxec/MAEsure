a6f0e4ba5b6c39cfd7c3495d8dca534451a951dd
Raphael Kriegl <raphael.kriegl@oth-regensburg.de>
2020-09-29 14:29:25 +0200

    angle calc done

--- evaluate_droplet.py
+++ evaluate_droplet.py
@@ -39,8 +39,10 @@ class Droplet():
         self.base_diam = 0
 
 def evaluate_droplet(img, y_base) -> Droplet:
-    
+    drplt = Droplet()
     crop_img = img[:y_base,:]
+    height, width, channels = img.shape
+    img = cv2.UMat(img)
     crop_img = cv2.UMat(crop_img)
     # calculate thrresholds
     #thresh_high, thresh_im = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
@@ -79,15 +81,34 @@ def evaluate_droplet(img, y_base) -> Droplet:
     x_int_l = min(intersection)
     x_int_r = max(intersection)
 
+    foc_len = math.sqrt(abs((MA/2)**2 - (ma/2)**2))
+    # find left focal point and get y coord
+    drplt.foc_pt1 = (x0 + foc_len*math.cos(phi), y0 + foc_len*math.sin(phi))
+    drplt.foc_pt2 = (x0 - foc_len*math.cos(phi), y0 - foc_len*math.sin(phi))
+    if drplt.foc_pt1[0] < drplt.foc_pt2[0]:
+        y_foc = drplt.foc_pt1[1]
+    else:
+        y_foc = drplt.foc_pt2[1]
+
+
+    # precalculations for slope of the tangents
     m_pre_l = (x_int_l*b**2 + e*b - 2*c*d - 4*a*c*x_int_l)/math.sqrt(b**2*x_int_l**2 + 2*b*e*x_int_l + e**2 - 4*a*c*x_int_l**2 - 4*c*d*x_int_l - 4*c*f)
-    m_t_l = -(b - m_pre_l)/(2*c)
-
-    angle_l = math.atan2(m_t_l, 1)
-
     m_pre_r = (x_int_r*b**2 + e*b - 2*c*d - 4*a*c*x_int_r)/math.sqrt(b**2*x_int_r**2 + 2*b*e*x_int_r + e**2 - 4*a*c*x_int_r**2 - 4*c*d*x_int_r - 4*c*f)
-    m_t_r = -(b - m_pre_r)/(2*c)
 
-    angle_r = math.atan2(m_t_r, 1)
+    if y_base > y_foc:
+        m_pre_l = -m_pre_l
+        m_pre_r = -m_pre_r
+    # if ellipse axes were wrong everything is mirrored
+    # if ax_swap:
+    #     m_pre_l = -m_pre_l
+    #     m_pre_r = -m_pre_r
+
+    # calc slope and angle of tangent
+    m_t_l = -(b - m_pre_l)/(2*c)
+    angle_l = math.atan(1/m_t_l) + math.pi/2
+ 
+    m_t_r = -(b - m_pre_r)/(2*c)
+    angle_r = math.atan(-1/m_t_r) + math.pi/2
 
     # ell = Ellipse((x0,y0), hradius=MA/2, vradius=ma/2).rotate(phi)
     # lin = Line((0,y_base), slope=0)
@@ -97,9 +118,6 @@ def evaluate_droplet(img, y_base) -> Droplet:
     # int_l = ell.tangent_lines(intersections[0])
     # int_r = ell.tangent_lines(intersections[1])
 
-    foc_len = math.sqrt(abs((MA/2)**2 - (ma/2)**2))
-
-    drplt = Droplet()
     drplt.angle_l = angle_l
     drplt.angle_r = angle_r
     drplt.maj = MA
@@ -110,9 +128,22 @@ def evaluate_droplet(img, y_base) -> Droplet:
     drplt.tan_r_m = m_t_r
     drplt.int_l = (x_int_l, y_base)
     drplt.int_r = (x_int_r, y_base)
-    drplt.foc_pt1 = (x0 + foc_len*math.cos(phi), y0 + foc_len*math.sin(phi))
-    drplt.foc_pt2 = (x0 - foc_len*math.cos(phi), y0 - foc_len*math.sin(phi))
+    
+    drplt.base_diam = x_int_r - x_int_l
 
+    # TODO
+    # test cv2 ellipse plot, line plot etc
+    img = cv2.ellipse(img, (int(round(x0)),int(round(y0))), (int(round(pa)),int(round(pb))), int(round(phi*180/math.pi)), 0, 360, (255,0,255), thickness=1, lineType=cv2.LINE_AA)
+    y_int = int(round(y_int))
+    img = cv2.line(img, (int(round(x_int_l - (y_int/m_t_l))), 0), (int(round(x_int_l + ((height - y_int)/m_t_l))), int(round(height))), (255,0,255), thickness=1, lineType=cv2.LINE_AA)
+    img = cv2.line(img, (int(round(x_int_r - (y_int/m_t_r))), 0), (int(round(x_int_r + ((height - y_int)/m_t_r))), int(round(height))), (255,0,255), thickness=1, lineType=cv2.LINE_AA)
+    img = cv2.ellipse(img, (int(round(x_int_l)),y_int), (20,20), 0, 0, -int(round(angle_l*180/math.pi)), (255,0,255), thickness=1, lineType=cv2.LINE_AA)
+    img = cv2.ellipse(img, (int(round(x_int_r)),y_int), (20,20), 0, 180, 180 + int(round(angle_r*180/math.pi)), (255,0,255), thickness=1, lineType=cv2.LINE_AA)
+    img = cv2.line(img, (0,y_int), (width, y_int), (255,0,0), thickness=2, lineType=cv2.LINE_AA)
+    img = cv2.putText(img, '<' + str(round(angle_l*180/math.pi,1)), (5,y_int-5), cv2.FONT_HERSHEY_COMPLEX, .5, (0,0,0))
+    img = cv2.putText(img, '<' + str(round(angle_r*180/math.pi,1)), (width - 80,y_int-5), cv2.FONT_HERSHEY_COMPLEX, .5, (0,0,0))
+    cv2.imshow('Heyho',img)
+    cv2.waitKey(0)
     return drplt
 
 
