2b27a8a94f9dbe2852e84ff6efdc2403b8beba69
Raphael Kriegl <raphael.kriegl@oth-regensburg.de>
2020-10-12 17:25:24 +0200

    implemented large parts of magnet control

--- magnet_control.py
+++ magnet_control.py
@@ -14,5 +14,81 @@
 #     You should have received a copy of the GNU General Public License
 #     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+from PySide2.QtCore import Signal, Slot, Qt
+
+from serial.tools import list_ports
+from lt_control.lt_control import LT
+
+# TODO
+# calib
+# lamp shows reference status
+
 class MagnetControl:
-    pass
+    def __init__(self, parent=None) -> None:
+        #self.portsComboBox: QComboBox = None
+        self._lt_ctl = LT(self.find_com_port())
+        self._shown = False
+        self._mov_dist: float = 0
+        self._mov_unit: str = 'steps'
+
+
+    def __del__(self):
+        self._lt_ctl.close()
+        del self._lt_ctl
+
+    def find_com_port(self) -> str:
+        lst = list_ports.comports()
+        for port in lst:
+            if port.manufacturer == 'Nanotec':
+                return port.device
+
+    def showEvent(self, event):
+        if not self._shown:
+            self.post_init()
+            self._shown = True
+
+    @Slot()
+    def jog_up_start(self):
+        self._lt_ctl.move_inf_start(0)
+
+    @Slot()
+    def jog_down_start(self):
+        self._lt_ctl.move_inf_start(1)
+        
+    def set_mov_dist(self, value: float):
+        try:
+            self._mov_dist = value   
+        except Exception as ex:
+            print(ex)
+
+    def set_mov_unit(self, text: str):
+        try:
+            self._mov_unit = text.strip()
+        except Exception as ex:
+            print(ex) 
+        
+    @Slot()
+    def move_pos(self):
+        # FIXME movement not correct!
+        if self._mov_unit == 'mm':
+            self._lt_ctl.move_absolute_mm(self._mov_dist)
+        elif self._mov_unit == 'steps':
+            self._lt_ctl.move_absolute(int(self._mov_dist))
+        elif self._mov_unit == 'mT':
+            print('Not implemented!')
+
+    @Slot()
+    def motor_stop(self):
+        self._lt_ctl.stop()
+
+    @Slot()
+    def reference(self):
+        self._lt_ctl.do_referencing()
+
+    def populate_int_selector(self):
+        pass
+
+    @Slot()
+    def connect(self):
+        pass
+
--- main_window.py
+++ main_window.py
@@ -18,14 +18,17 @@
 import os
 import atexit
 
-from PySide2.QtWidgets import QMainWindow
-from PySide2.QtCore import QFile, Signal
+from PySide2.QtWidgets import QComboBox, QMainWindow
+from PySide2.QtCore import QFile, Signal, Slot
 from PySide2.QtUiTools import QUiLoader
 #from ui_main import Ui_main
 
 from camera_control import CameraControl
 from magnet_control import MagnetControl
 from pump_control import PumpControl
+from data_control import DataControl
+from measurement_control import MeasurementControl
+from light_widget import LightWidget
 
 class MainWindow(QMainWindow):
     start_acquisition_signal = Signal()
@@ -38,6 +41,8 @@ class MainWindow(QMainWindow):
         file = QFile("./form.ui")
         file.open(QFile.ReadOnly)
         loader.registerCustomWidget(CameraControl)
+        loader.registerCustomWidget(DataControl)
+        loader.registerCustomWidget(LightWidget)
         self.ui = loader.load(file)
         file.close()
         self.setCentralWidget(self.ui)
@@ -45,11 +50,12 @@ class MainWindow(QMainWindow):
         # self.ui = Ui_main()
         # self.ui.setupUi(self)
         atexit.register(self.cleanup)
-        self.ui.btn_prev_st.clicked.connect(self.prev_start_pushed)
-        self.ui.btn_set_roi.clicked.connect(self.ui.camera_prev.apply_roi)
-        self.ui.btn_reset_roi.clicked.connect(self.ui.camera_prev.reset_roi)
-        self.start_acquisition_signal.connect(self.ui.camera_prev.start_preview)
-        self.stop_acquisition_signal.connect(self.ui.camera_prev.stop_preview)
+        self.magnet_ctl = MagnetControl()
+        self.meas_ctl = MeasurementControl()
+        self.pump_ctl = PumpControl()
+
+        self.connect_signals()
+        
         self.show()
         
 
@@ -59,6 +65,25 @@ class MainWindow(QMainWindow):
     def cleanup(self):
         del self
 
+    def connect_signals(self):
+        # Camera
+        self.ui.btn_prev_st.clicked.connect(self.prev_start_pushed)
+        self.ui.btn_set_roi.clicked.connect(self.ui.camera_prev.apply_roi)
+        self.ui.btn_reset_roi.clicked.connect(self.ui.camera_prev.reset_roi)
+        self.start_acquisition_signal.connect(self.ui.camera_prev.start_preview)
+        self.stop_acquisition_signal.connect(self.ui.camera_prev.stop_preview)
+
+        # Magnet
+        self.ui.jogUpBtn.pressed.connect(self.magnet_ctl.jog_up_start)
+        self.ui.jogDownBtn.pressed.connect(self.magnet_ctl.jog_down_start)
+        self.ui.jogUpBtn.released.connect(self.magnet_ctl.motor_stop)
+        self.ui.jogDownBtn.released.connect(self.magnet_ctl.motor_stop)
+        self.ui.referenceBtn.clicked.connect(self.magnet_ctl.reference)
+        self.ui.goBtn.clicked.connect(self.magnet_ctl.move_pos)
+        self.ui.posSpinBox.valueChanged.connect(self.spin_box_val_changed) #lambda pos: self.magnet_ctl.set_mov_dist(int(pos)) or self.ui.posSlider.setValue(int(pos))
+        self.ui.unitComboBox.currentTextChanged.connect(self.mag_mov_unit_changed)
+        self.ui.posSlider.sliderMoved.connect(self.slider_moved) # lambda pos: self.ui.posLineEdit.setText(str(pos)) or self.ui.posSpinBox.setValue(float(pos))
+
     def load_ui(self):
         loader = QUiLoader()
         path = os.path.join(os.path.dirname(__file__), "form.ui")
@@ -66,7 +91,38 @@ class MainWindow(QMainWindow):
         ui_file.open(QFile.ReadOnly)
         loader.load(ui_file, self)
         ui_file.close()
+    
+    @Slot(str)
+    def mag_mov_unit_changed(self, unit: str):
+        self.magnet_ctl.set_mov_unit(unit)
+        if unit == 'mm':
+            self.ui.posSlider.setMaximum(3906) #max mm are 39.0625
+            self.ui.posSlider.setTickInterval(100)
+            self.ui.posSpinBox.setDecimals(2)
+            self.ui.posSpinBox.setValue(self.magnet_ctl._lt_ctl.steps_to_mm(self.ui.posSpinBox.value()))
+        elif unit == 'steps':
+            self.ui.posSlider.setMaximum(50000)
+            self.ui.posSlider.setTickInterval(1000)
+            self.ui.posSpinBox.setDecimals(0)
+            self.ui.posSpinBox.setValue(self.magnet_ctl._lt_ctl.mm_to_steps(self.ui.posSpinBox.value()))
 
+    @Slot(float)
+    def spin_box_val_changed(self, value: float):
+        if self.ui.unitComboBox.currentText() == 'steps':
+            self.magnet_ctl.set_mov_dist(int(value))
+            self.ui.posSlider.setValue(int(value))
+        elif self.ui.unitComboBox.currentText() == 'mm':
+            self.magnet_ctl.set_mov_dist(value)
+            self.ui.posSlider.setValue(int(value*100))
+
+    @Slot(int)
+    def slider_moved(self, value: int):
+        if self.ui.unitComboBox.currentText() == 'steps':
+            self.ui.posSpinBox.setValue(float(value))
+        elif self.ui.unitComboBox.currentText() == 'mm':
+            self.ui.posSpinBox.setValue(float(value/100))
+
+    @Slot()
     def prev_start_pushed(self, event):
         if self.ui.btn_prev_st.text() != 'Stop':
             self.start_acquisition_signal.emit()
