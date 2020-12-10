ee883ce71f7e5612130fd89e39d03ac5113441d5
Raphael <40037381+rk-exxec@users.noreply.github.com>
2020-12-10 12:17:52 +0100

    fix rubberband

--- resizable_rubberband.py
+++ resizable_rubberband.py
@@ -20,10 +20,13 @@ from PySide2.QtWidgets import QWidget, QHBoxLayout, QSizeGrip, QRubberBand
 from PySide2.QtGui import QPainter, QPen
 from PySide2.QtCore import Qt, QPoint
 
+# FIXME broken handles
+
 class ResizableRubberBand(QWidget):
     def __init__(self, parent=None):
         super(ResizableRubberBand, self).__init__(parent)
-        #self.setWindowFlags(Qt.SubWindow)
+        self.setWindowFlag(Qt.SubWindow)
+        self.setFocusPolicy(Qt.ClickFocus)
         self.layout = QHBoxLayout(self)
         self.layout.setContentsMargins(0, 0, 0, 0)
         self.setCursor(Qt.SizeAllCursor)
